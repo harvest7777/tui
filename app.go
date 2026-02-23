fddfdae49b44e2f5e0b67093d195590aaa46cb6a
{harvest7777} <{333harvest.moon333@gmail.com}>
2026-02-22 23:05:26 -0800

    feat: epic first package

---
+++ app.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/harvest7777/tui/math"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	var x int = math.Add(3, 2)
+	fmt.Println(x)
+}
---
+++ math/helpers.go
@@ -0,0 +1,7 @@
+package math
+
+// capitalized is visible in other packages
+// kinda like hwo static works in c
+func Add(a int, b int) int {
+	return a + b
+}
